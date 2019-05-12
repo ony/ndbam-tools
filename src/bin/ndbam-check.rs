17d97a176497732120fb51cc6bdaf30332c8434f
Mykola Orliuk <virkony@gmail.com>
2019-05-13 00:07:44 +0200

    ndbam-check: verify files integrity

--- src/bin/ndbam-check.rs
+++ src/bin/ndbam-check.rs
@@ -1,9 +1,13 @@
 extern crate structopt;
 
 use std::time::{SystemTime, UNIX_EPOCH};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::fs;
+use std::io;
+use std::io::{BufRead, Write};
 use ndbam::*;
 use ndbam::contents::*;
+use crypto_hash::{Algorithm, Hasher};
 use structopt::StructOpt;
 use colored::*;
 
@@ -21,17 +25,20 @@ struct Opts {
     #[structopt(long = "allow-mtime")]
     allow_mtime: bool,
 
+    /// Skip file integrity checking
+    #[structopt(long = "no-integrity")]
+    no_integrity: bool,
+
     #[structopt(short = "v", long = "verbose")]
     verbose: bool,
 }
 
 fn main() {
     let opts = Opts::from_args();
-    println!("Hello, Exherbo!");
     let reg = NDBAM::new(&opts.location);
     reg.all_packages()
         .map(|iter|
-             iter.for_each(|pkg| {
+             for pkg in iter {
                  let mut header_shown = false;
                  let mut header = || {
                      if header_shown { return }
@@ -49,7 +56,7 @@ fn main() {
                  if !opts.no_contents {
                      check_contents(&opts, &pkg, &mut header)
                  }
-             })
+             }
         );
 }
 
@@ -95,22 +102,61 @@ fn check_contents(opts: &Opts, pkg: &PackageView, header: &mut impl FnMut()) {
                     continue;
                 }
             },
-            Entry::File { .. } => {
+
+            Entry::File { ref md5, .. } => {
                 if !metadata.is_file() {
                     header();
                     println!("  T {} Not a regular file", path.to_string_lossy().red());
                     continue;
                 }
-                // TODO: check MD5
+
+                if !opts.no_integrity {
+                    match file_md5(path) {
+                        Ok(real_md5) => {
+                            if &real_md5 != md5 {
+                                header();
+                                println!("  C {} Content changed", path.to_string_lossy().red());
+                                continue;
+                            }
+                        },
+                        Err(err) => {
+                            println!("  X {} {}", path.to_string_lossy().red(), err.to_string());
+                        },
+                    }
+                }
             },
-            Entry::Sym { .. } => {
+
+            Entry::Sym { ref target, .. } => {
                 if !metadata.file_type().is_symlink() {
                     header();
                     println!("  T {} Not a symbolic link", path.to_string_lossy().red());
                     continue;
                 }
-                // TODO: check target
-                // TODO: check dangling
+
+                match path.read_link() {
+                    Ok(real_target) => {
+                        if *target != real_target {
+                            header();
+                            println!("  C {} Symlink changed", path.to_string_lossy().red());
+                            continue;
+                        }
+                    },
+                    Err(err) => {
+                        header();
+                        println!("  X {} {}", path.to_string_lossy().red(), err.to_string());
+                        continue;
+                    },
+                }
+
+                if let Err(err) = path.canonicalize() {
+                    header();
+                    if err.kind() == std::io::ErrorKind::NotFound {
+                        println!("  X {} Dangling symbolic link", path.to_string_lossy().red());
+                    } else {
+                        println!("  X {} {}", path.to_string_lossy().red(), err.to_string());
+                    }
+                    continue;
+                }
             },
         }
     }
@@ -118,4 +164,19 @@ fn check_contents(opts: &Opts, pkg: &PackageView, header: &mut impl FnMut()) {
 
 fn epoch_secs(moment: &SystemTime) -> u64 {
     moment.duration_since(UNIX_EPOCH).unwrap().as_secs()
+}
+
+fn file_md5(path: &Path) -> io::Result<String> {
+    let mut reader = io::BufReader::new(fs::File::open(path)?);
+    let mut hasher = Hasher::new(Algorithm::MD5);
+    loop {
+        let chunk = reader.fill_buf()?;
+        if chunk.is_empty() {
+            return Ok(hex::encode(hasher.finish()))
+        }
+        hasher.write_all(chunk)?;
+
+        let n = chunk.len();
+        reader.consume(n);
+    }
 }
