84f78275cf1f15554b805cf51b47d79d5ba20ba4
Mykola Orliuk <virkony@gmail.com>
2019-05-18 12:16:52 +0200

    tests: extract content_steps mod
    Apparently M.rs and M/mod.rs have different meaning for tests in Rust.

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -70,39 +70,7 @@ mod basic_steps {
     });
 }
 
-mod content_steps {
-    use super::*;
-    use std::fs;
-
-    steps!(super::Env => {
-        given regex r"^file (.+)$" (PathBuf) |world, path, step| {
-            let real_path = world.real_path(&path);
-            create_dir_for(&real_path);
-            if let Some(content) = step.docstring() {
-                assert!(!content.contains('<') && !content.contains('>'),
-                    "variables are not yet supported. cucumber test skipped"); // magic trail to skip
-                fs::write(&real_path, content)
-            } else {
-                fs::write(&real_path, "dummy")
-            }.expect(format!("write to {:?} (original {:?})", &real_path, &path).as_str());
-        };
-
-        given regex r"^directory (.+)$" (PathBuf) |world, path, _step| {
-            fs::create_dir_all(world.real_path(&path)).unwrap();
-        };
-
-        given regex r"^symlink (.+) to (.+)$" (PathBuf, PathBuf) |world, path, target, _step| {
-            let real_path = world.real_path(&path);
-            create_dir_for(&real_path);
-            fs::soft_link(&target, &real_path)
-                .expect(format!("symlink at {:?} {:?}", &real_path, &path).as_str());
-        };
-    });
-
-    fn create_dir_for(path: &Path) {
-        path.parent().map(|parent| fs::create_dir_all(parent).unwrap() );
-    }
-}
+mod content_steps;
 
 cucumber! {
     features: "./features",
